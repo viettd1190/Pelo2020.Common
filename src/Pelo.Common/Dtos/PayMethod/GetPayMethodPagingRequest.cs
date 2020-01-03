90e205274162e7464891746b3b6afaf840547bbb
viettd <viettd1190@gmail.com>
2020-01-03 23:22:48 +0700

    Get all product unit, pay method, invoice status

---
+++ src/Pelo.Common/Dtos/PayMethod/GetPayMethodPagingRequest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Pelo.Common.Models;
+
+namespace Pelo.Common.Dtos.PayMethod
+{
+    public class GetPayMethodPagingRequest : GetPagingModel
+    {
+        [JsonProperty("name")]
+        [FromQuery(Name = "name")]
+        public string Name { get; set; }
+    }
+}
---
+++ src/Pelo.Common/Dtos/PayMethod/InsertPayMethodRequest.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Pelo.Common.Dtos.PayMethod
+{
+    public class InsertPayMethodRequest
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
