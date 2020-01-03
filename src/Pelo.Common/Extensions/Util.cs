e74a0d83e6fd429d91866818904ab0febea0e324
tan tran <tantranvan9463@gmail.com>
2020-01-04 01:40:21 +0700

    update

---
+++ src/Pelo.Common/Extensions/Util.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pelo.Common.Extensions
+{
+    public class Util
+    {
+        public static List<int> GetArrays(string data)
+        {
+            if (!string.IsNullOrEmpty(data))
+            {
+                List<int> list = new List<int>();
+                string[] arr = data.Split(new[] { ',', ';' });
+                foreach (var item in arr)
+                {
+                    int i = 0;
+                    if (!string.IsNullOrEmpty(item) && int.TryParse(item,out i))
+                    {
+                        list.Add(i);
+                    }
+                }
+            }
+            return new List<int>();
+        }
+    }
+}
