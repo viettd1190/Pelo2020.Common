90df01b8467d67caaca43cd19efab3f7192a81a8
tan tran <tantranvan9463@gmail.com>
2020-01-09 00:54:42 +0700

    update

---
+++ src/Pelo.Common/Dtos/Crm/CrmUserResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pelo.Common.Dtos.Crm
+{
+    public class CrmUserResponse
+    {
+        public int Id { get; set; }
+
+        public int CrmId { get; set; }
+
+        public int UserId { get; set; }
+
+        public int Type { get; set; }
+
+        public int UserCreated { get; set; }
+
+        public int UserUpdated { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateUpdated { get; set; }
+
+    }
+}
