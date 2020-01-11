c71bbc4d738e5338d351fee16e3635b8fad67ff9
tan tran <tantranvan9463@gmail.com>
2020-01-11 12:15:17 +0700

    update

---
+++ src/Pelo.Common/Dtos/Crm/CommentCrmRequest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pelo.Common.Dtos.Crm
+{
+    public class CommentCrmRequest
+    {
+        public int Id { get; set; }
+
+        public int CrmStatusId { get; set; }
+
+        public string Comment { get; set; }
+
+        public IFormFile File { get; set; }
+    }
+}
