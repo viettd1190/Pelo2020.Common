ac89b333f4014ada046bdd5c121d15b7c8532ae0
tan tran <tantranvan9463@gmail.com>
2020-01-14 00:01:16 +0700

    update crmstatus

---
+++ src/Pelo.Common/Dtos/CrmStatus/GetCrmStatusResponse.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pelo.Common.Dtos.CrmPriority
+{
+    public class GetCrmStatusResponse
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("color")]
+        public string Color { get; set; }
+
+        [JsonProperty("send_sms")]
+        public bool IsSendSms { get; set; }
+
+        [JsonProperty("sms_content")]
+        public string SmsContent { get; set; }
+    }
+}
---
+++ src/Pelo.Common/Dtos/CrmStatus/UpdateCrmStatus.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pelo.Common.Dtos.CrmPriority
+{
+    public class UpdateCrmStatus
+    {
+        [JsonProperty("id")]
+        [FromQuery(Name = "id")]
+        public int Id { get; set; }
+
+        [JsonProperty("color")]
+        [FromQuery(Name = "color")]
+        public string Color { get; set; }
+
+        [JsonProperty("name")]
+        [FromQuery(Name = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty("send_sms")]
+        [FromQuery(Name = "send_sms")]
+        public bool IsSendSms { get; set; }
+
+        [JsonProperty("sms_content")]
+        [FromQuery(Name = "sms_content")]
+        public string SmsContent { get; set; }
+
+    }
+}
