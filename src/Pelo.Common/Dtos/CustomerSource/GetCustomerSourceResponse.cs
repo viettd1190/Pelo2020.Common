64d8fb1272411dc43135bd3c398e63c040936600
tan tran <tantranvan9463@gmail.com>
2020-01-14 00:54:53 +0700

    update customer source

---
+++ src/Pelo.Common/Dtos/CustomerSource/GetCustomerSourceResponse.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pelo.Common.Dtos.CustomerSource
+{
+    public class GetCustomerSourceResponse
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+    }
+}
---
+++ src/Pelo.Common/Dtos/CustomerSource/UpdateCustomerSource.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pelo.Common.Dtos.CustomerSource
+{
+    public class UpdateCustomerSource
+    {
+        [JsonProperty("id")]
+        [FromQuery(Name = "id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        [FromQuery(Name = "name")]
+        public string Name { get; set; }
+
+    }
+}
