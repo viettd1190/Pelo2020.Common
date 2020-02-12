1185b37156470f90b237bf7c0528cff9effab3c3
tan tran <tantranvan9463@gmail.com>
2020-02-12 23:07:30 +0700

    update WARRANTY_DESCRIPTION

---
+++ src/Pelo.Common/Dtos/WarrantyDescription/InsertWarrantyDescription.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pelo.Common.Dtos.WarrantyDescription
+{
+    public class InsertWarrantyDescription
+    {
+        [JsonProperty("name")]
+        [FromQuery(Name = "name")]
+        public string Name { get; set; }
+
+    }
+}
