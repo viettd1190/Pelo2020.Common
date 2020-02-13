a56bdb8b246b01143af3871a198ff64fa9a514eb
viettd <viettd1190@gmail.com>
2020-02-13 09:48:12 +0700

    Refactor code

--- src/Pelo.Common/Dtos/Department/GetDepartmentPagingRequest.cs
+++ src/Pelo.Common/Dtos/Department/GetDepartmentPagingRequest.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Pelo.Common.Models;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Pelo.Common.Dtos.Department
 {
