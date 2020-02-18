211e881f75805d0ccd24dcd145e3f51566206f0e
Việt Trần <viettd1190@gmail.com>
2020-02-18 17:10:26 +0700

    Update PeloException

--- src/Pelo.Common/Exceptions/PeloException.cs
+++ src/Pelo.Common/Exceptions/PeloException.cs
@@ -9,6 +9,21 @@ namespace Pelo.Common.Exceptions
             Msg = message;
         }
 
+        /// <summary>
+        ///     Initializes a new instance of the Exception class with a specified error message and a reference to the inner
+        ///     exception that is the cause of this exception.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="innerException">
+        ///     The exception that is the cause of the current exception, or a null reference if no inner
+        ///     exception is specified.
+        /// </param>
+        public PeloException(string message,
+                             Exception innerException)
+                : base(message, innerException)
+        {
+        }
+
         public string Msg { get; set; }
     }
 }
