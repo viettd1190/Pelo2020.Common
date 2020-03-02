0792b9d5622f4b8cccd1fa8d514bb3e94b2460e6
Việt Trần <viettd1190@gmail.com>
2020-03-03 00:10:53 +0700

    Add event crm insert success

---
+++ src/Pelo.Common/Events/Crm/CrmInsertSuccessEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Pelo.Common.Events.Crm
+{
+    public class CrmInsertSuccessEvent : IEvent
+    {
+        public int Id { get; set; }
+
+        public string Code { get; set; }
+
+        public int UserId { get; set; }
+
+        public int CrmStatusId { get; set; }
+
+        /// <summary>
+        ///     Danh sách user nhận thông báo khi crm mới được thêm
+        /// </summary>
+        public List<int> UserIds { get; set; }
+    }
+}
