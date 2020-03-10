53a9c76be6aae58c2ad5c0e2a86d9d4cfad1ecbb
Việt Trần <viettd1190@gmail.com>
2020-03-10 16:30:29 +0700

    Add invoice insert success event

---
+++ src/Pelo.Common/Events/Invoice/InvoiceInsertSuccessEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace Pelo.Common.Events.Invoice
+{
+    public class InvoiceInsertSuccessEvent : IEvent
+    {
+        public int Id { get; set; }
+
+        public string Code { get; set; }
+    }
+}
