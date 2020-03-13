0b2beb5758244faa222700d7805aee7fb6c3b324
Việt Trần <viettd1190@gmail.com>
2020-03-13 15:22:54 +0700

    Add event crm comment success

---
+++ src/Pelo.Common/Events/Crm/CrmCommentSuccessEvent.cs
@@ -0,0 +1,17 @@
+﻿namespace Pelo.Common.Events.Crm
+{
+    public class CrmCommentSuccessEvent : IEvent
+    {
+        public int Id { get; set; }
+
+        public string Code { get; set; }
+
+        public int CrmStatusId { get; set; }
+
+        public int UserId { get; set; }
+
+        public string Comment { get; set; }
+
+        public bool HasAttachmentFile { get; set; }
+    }
+}
