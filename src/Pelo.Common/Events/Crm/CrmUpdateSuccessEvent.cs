2f97c81c4ed3043d67ad2eabf1cbb13b19764fdc
Việt Trần <viettd1190@gmail.com>
2020-03-16 14:10:51 +0700

    Add event update crm success

---
+++ src/Pelo.Common/Events/Crm/CrmUpdateSuccessEvent.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pelo.Common.Events.Crm
+{
+    public class CrmUpdateSuccessEvent : IEvent
+    {
+        public CrmUpdateSuccessModel BeforeUpdated { get; set; }
+
+        public CrmUpdateSuccessModel AfterUpdated { get; set; }
+
+        public int Id { get; set; }
+
+        public string Code { get; set; }
+
+        public int UserId { get; set; }
+
+        public string Reason { get; set; }
+    }
+
+    public class CrmUpdateSuccessModel
+    {
+        public int CrmStatusId { get; set; }
+
+        public int ProductGroupId { get; set; }
+
+        public int CrmPriorityId { get; set; }
+
+        public int CrmTypeId { get; set; }
+
+        public int CustomerSourceId { get; set; }
+
+        public List<int> UserIds { get; set; }
+
+        public string Need { get; set; }
+
+        public string Description { get; set; }
+
+        public int Visit { get; set; }
+
+        public DateTime ContactDate { get; set; }
+    }
+}
