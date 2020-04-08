9943292fea5ad1bc70b8c35301661a1dcaeebb68
tan tran <tantranvan9463@gmail.com>
2020-04-08 23:34:36 +0700

    update recruitmentstatus

--- src/Pelo.Common/Dtos/RecruitmentStatus/RecruitmentStatusResponse.cs
+++ src/Pelo.Common/Dtos/RecruitmentStatus/RecruitmentStatusResponse.cs
@@ -9,19 +9,15 @@ namespace Pelo.Common.Dtos.RecruitmentStatus
     public class RecruitmentStatusResponse
     {
         [JsonProperty("id")]
-        [FromQuery(Name = "id")]
         public int Id { get; set; }
 
         [JsonProperty("color")]
-        [FromQuery(Name = "color")]
         public string Color { get; set; }
 
         [JsonProperty("name")]
-        [FromQuery(Name = "name")]
         public string Name { get; set; }
 
         [JsonProperty("sort_order")]
-        [FromQuery(Name = "sort_order")]
         public int SortOrder { get; set; }
     }
 }
