c906c14cdcc4da1daf5d5bb95365bb4c5b1655fa
tan tran <tantranvan9463@gmail.com>
2020-04-20 22:57:24 +0700

    warranty status

--- src/Pelo.Common/Dtos/WarrantyStatus/GetIWarrantyStatusPagingResponse.cs
+++ src/Pelo.Common/Dtos/WarrantyStatus/GetIWarrantyStatusPagingResponse.cs
@@ -15,7 +15,7 @@ namespace Pelo.Common.Dtos.WarrantyStatus
 
         [JsonProperty("sort_order")] public int SortOrder { get; set; }
 
-        [JsonProperty("send_sms")] public bool IsSendSms { get; set; }
+        [JsonProperty("is_send_sms")] public bool IsSendSms { get; set; }
 
         [JsonProperty("sms_content")] public string SmsContent { get; set; }
 
--- src/Pelo.Common/Dtos/WarrantyStatus/GetWarrantyStatusResponse.cs
+++ src/Pelo.Common/Dtos/WarrantyStatus/GetWarrantyStatusResponse.cs
@@ -12,21 +12,20 @@ namespace Pelo.Common.Dtos.WarrantyStatus
         public int Id { get; set; }
 
         [JsonProperty("color")]
-        [FromQuery(Name = "color")]
         public string Color { get; set; }
 
         [JsonProperty("name")]
-        [FromQuery(Name = "name")]
         public string Name { get; set; }
 
         [JsonProperty("sort_order")]
-        [FromQuery(Name = "sort_order")]
         public int SortOrder { get; set; }
 
 
-        [JsonProperty("is_send_sms")] [FromQuery(Name = "is_send_sms")] public bool IsSendSms { get; set; }
+        [JsonProperty("is_send_sms")]
+        public bool IsSendSms { get; set; }
 
-        [JsonProperty("sms_content")] [FromQuery(Name = "sms_content")] public string SmsContent { get; set; }
+        [JsonProperty("sms_content")]
+        public string SmsContent { get; set; }
 
     }
 }
